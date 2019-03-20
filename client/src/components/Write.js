f43e082eb8db43697291f792522bc9496ee1c3a9
findaw <wqre1@naver.com>
2019-03-20 16:24:42 +0900

    Add Tag Field & Handle AddButton

--- client/src/components/Write.js
+++ client/src/components/Write.js
@@ -10,6 +10,11 @@ import Checkbox from '@material-ui/core/Checkbox';
 import InputBase from '@material-ui/core/InputBase';
 import Button from '@material-ui/core/Button';
 import Badge from '@material-ui/core/Badge';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+
+import TagFacesIcon from '@material-ui/icons/TagFaces';
 
 const styles = theme=>({
     container:{
@@ -18,25 +23,43 @@ const styles = theme=>({
         marginBottom : 22,
         marginTop : 22,
     },
-    writeBox: {
+    contentField: {
         marginTop : 30,
     },
     textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
     },
     button: {
         marginTop : 25,
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    },
+    tagField:{
+        marginTop:20,
+    },
+    tagPaper: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        padding: theme.spacing.unit / 2,
+    },
+    tag: {
+        margin: theme.spacing.unit / 2,
     },
 });
 class Write extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            text : ["주제1", "주제2", "주제3", "주제4"],
+            topic : ["주제1", "주제2", "주제3", "주제4"],
             check : [false,false,false,false],
             need : [true,false,false,false],
             content : ["","","",""],
+            tagData: ['Angular','jQuery','Polymer','React','Vue.js'],
+            addTag:"",
+            addTopic:"",
         };
     }
     handleCheckbox = index =>{
@@ -48,18 +71,91 @@ class Write extends React.Component{
         this.setState({
             content : this.state.content.map((v,i) => i === index ? event.target.value : v)
         });
-        console.log(event.target.value);
-        console.log(this.state.content);
-        if((event.target.value.trim() != "" && !this.state.check[index])
+
+        if((event.target.value.trim() !== "" && !this.state.check[index])
         || (event.target.value.trim() === "" && this.state.check[index])){
             this.handleCheckbox(index);
         }
-        else {
-            return;
+    }
+    handleTopicAddButton = () =>{
+        this.setState(({topic,check,need,content})=>({
+            topic : [...topic,this.state.addTopic],
+            check : [...check,false],
+            need : [...need,false],
+            content : [...content,""],
+            addTopic : "",
+        }));
+    }
+    handleTopicFieldChange = event => {
+        this.setState({
+             addTopic : event.target.value,
+        });   
+    }
+    handleTagAddButton = () =>{
+        this.setState({
+            tagData : [...this.state.tagData,this.state.addTag],
+            addTag : "",
+        });
+    }
+    handleTagFieldChange = event => {
+        this.setState({
+             addTag : event.target.value,
+        });
+    }
+    handleTagDelete = data => () => {
+        if (data.label === 'React') {
+          alert('Why would you want to delete React?! :)'); 
+          return;
+        }
+        const chipData = [...this.state.tagData];
+        const chipToDelete = chipData.indexOf(data);
+        chipData.splice(chipToDelete, 1);
+
+        this.setState({tagData : chipData});
+    };
+    handleSaveButton = () => {
+        let flag = 0;
+        let text = "";
+        this.state.need.forEach((data,index) => {
+            if(data && !this.state.check[index]){
+                text +="/" + this.state.topic[index];
+                flag++;
+                return;
+            }
+        });
+        if(flag === 0){
+            alert("저장되었습니다.");
+        }else{
+            alert(`필수 항목을 모두 입력해주세요.(${text})`);
         }
     }
     render(){
         const { classes } = this.props;
+        const contentBox  =
+            this.state.topic.map((value, index)=>{
+                return (
+                    <ExpansionPanel key={index}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>
+                                <Checkbox indeterminate={this.state.check[index]} checked={this.state.check[index]}/>
+                                {this.state.need[index] ? ( <Badge color="secondary" variant="dot">{value}&nbsp;&nbsp;</Badge>) : value}
+                            </Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <InputBase 
+                                label="content"
+                                multiline
+                                rows="15"
+                                fullWidth
+                                value={this.state.content[index]}
+                                onChange={this.handleContentChange(index)}
+                                placeholder="내용 입력"
+                                className={classes.textField}
+                            />
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                );
+            });
         return(
             <div className={classes.container}>
                 
@@ -74,39 +170,65 @@ class Write extends React.Component{
                     shrink: true,
                     }}
                 />
-                <div className={classes.writeBox}>
-                   {
-                       this.state.text.map((value, index)=>{
-                        return (
-                            <ExpansionPanel key={index}>
-                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                                
-                                    <Typography className={classes.heading}>
-                                        <Checkbox indeterminate={this.state.check[index]} checked={this.state.check[index]}/>
-                                        {this.state.need[index] ? ( <Badge color="secondary" variant="dot">{value}&nbsp;&nbsp;</Badge>) : value}
-                                    </Typography>
-                                
-                                </ExpansionPanelSummary>
-                                <ExpansionPanelDetails id="value">
-                                    <InputBase 
-                                        label="content"
-                                        multiline
-                                        rows="20"
-                                        fullWidth
-                                        value={this.state.content[index]}
-                                        onChange={this.handleContentChange(index)}
-                                        placeholder="내용 입력"
-                                        className={classes.textField}
-                                    />
-                                </ExpansionPanelDetails>
-                            </ExpansionPanel>
-                        );
-                       })
+                <div className={classes.tagField}>
+                    <Paper className={classes.tagPaper}>
+                        {this.state.tagData.map((data,index) => {
+                        let icon = null;
 
+                        if (data === 'React') {
+                            icon = <TagFacesIcon />;
+                        }
+
+                        return (
+                            <Chip
+                            key={index}
+                            icon={icon}
+                            label={data}
+                            onDelete={this.handleTagDelete(data)}
+                            className={classes.tag}
+                            />
+                        );
+                        })}
+                    </Paper>
+                    <TextField
+                        id="standard-with-placeholder"
+                        label="태그명"
+                        placeholder="관련 태그 입력"
+                        className={classes.textField}
+                        value={this.state.addTag}
+                        onChange={this.handleTagFieldChange}
+                        margin="normal"
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                    />
+                    <Button onClick={this.handleTagAddButton} variant="contained" color="primary" className={classes.button}>
+                        태그추가<AddCircleOutlineIcon className={classes.rightIcon} />
+                    </Button><br/>
+                </div>
+                <div className={classes.contentField}>
+                   {
+                       contentBox
                    }
                 </div>
-
-                <Button variant="contained" color="primary" className={classes.button}>저장</Button>
+                <div>
+                    <TextField
+                        id="standard-with-placeholder"
+                        label="주제명"
+                        placeholder="추가할 항목 이름 입력"
+                        className={classes.textField}
+                        value={this.state.addTopic}
+                        onChange={this.handleTopicFieldChange}
+                        margin="normal"
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                    />
+                    <Button onClick={this.handleTopicAddButton} variant="contained" color="primary" className={classes.button}>
+                        항목추가<AddCircleOutlineIcon className={classes.rightIcon} />
+                    </Button><br/>
+                </div>
+                <Button onClick={this.handleSaveButton}variant="contained" color="primary" className={classes.button}>저장</Button>
             </div>
         );
     }
--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.post('/signin', (req, res) => {
     const { username, password } = req.body;
     // 클라이언트로부터 전송된 페이로드를 그대로 response한다.
     res.send({ username, password });
-  });
+});
 
 app.listen(5000, ()=> console.log('listening on port 3000'));
