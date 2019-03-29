7377f997e98817387528f9a8579b8d1f8a686964
findaw <wqre1@naver.com>
2019-03-29 11:01:12 +0900

    DocumentWrite Module UI Working

--- client/src/components/DocumentWrite.js
+++ client/src/components/DocumentWrite.js
@@ -3,7 +3,6 @@ import CheckBoxList from './CheckBoxList';
 import RadioList from './RadioList';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField'
-import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
@@ -18,72 +17,6 @@ import InputBase from '@material-ui/core/InputBase';
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
-
-const styles = theme =>({
-    container:{
-        margin : 10,
-    },
-    titleField: {
-        marginTop : 30,
-        marginLeft: theme.spacing.unit*2,
-        marginRight: theme.spacing.unit*2,
-        width:1200,
-    },
-    titleComponenet:{
-        display:'flex',
-        justifyContent :'center',
-    },
-    selectComponent:{
-        display:'flex',
-        justifyContent :'center',
-        margin:20,
-    },
-    selectControl: {
-        margin : theme.spacing.unit*2,
-        width:1200,
-    },
-    subjectComponent :{
-        display:'flex',
-        justifyContent:'center'
-    },
-    subjectWrapper :{
-        width:1200,
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-    },
-    subjectField:{
-        width:'100%',
-    },
-    contentField:{
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-    },
-    checkBoxComponent:{
-        display:'flex',
-        flexWrap : 'wrap',
-        margin : theme.spacing.unit,
-        marginTop : 30,
-    },
-    tagComponent:{
-        display:'flex',
-        justifyContent:'center',
-    },
-    tagWrapper:{
-        width:1200,
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-    },
-    tagPaper:{
-        width:'100%',
-        
-    },
-    tagAddButton:{
-        height:58,
-    },
-    chip: {
-        margin: theme.spacing.unit,
-    },
-});
 
 class DocumentWrite extends React.Component{
     state={
@@ -131,17 +64,16 @@ class DocumentWrite extends React.Component{
             })
             .catch(err=>console.log(err));
     }
-    handleSelectChange = title=> (event,value) => {
+    handleSelectChange = (event) => {
         const {content, doctypeValue, subjects} = this.state;
-        console.log(event.target);
         this.setState({ subjects : {...subjects,
             [doctypeValue] : {
                 ...subjects[doctypeValue],
-                [title] : !subjects[doctypeValue][title]
+                [event.target.value] : !subjects[doctypeValue][event.target.value]
             }, 
         }});
-        if(value && !content[title])
-            this.setState({ content: {...content, [title] : ""} });
+        if(subjects[doctypeValue][event.target.value] && !content[event.target.value])
+            this.setState({ content: {...content, [event.target.value] : ""} });
     };
     handleSubjectBoxChange = panel => (event, expanded) => {
         this.setState({
@@ -163,7 +95,12 @@ class DocumentWrite extends React.Component{
         this.setState({
             [target] : event.target.value, 
             content : {}
+        },()=>{
+            let textareas = document.getElementsByTagName("textarea");
+            for(let item of textareas)
+                item.value = "";
         });
+
     }
     handleCheckBoxChange = target=> (key,key2)=> (event,value) => {
         const {addDefaultTags} = this.state;
@@ -209,29 +146,12 @@ class DocumentWrite extends React.Component{
     }
     render(){
         const {classes} = this.props;
-        const {doctype, doctypeValue, subjects,expanded, content,category, addDefaultTags, addTags } = this.state;
-        const ITEM_HEIGHT = 48;
-        const ITEM_PADDING_TOP = 8;
-        const MenuProps = {
-            PaperProps: {
-              style: {
-                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-                width: 250,
-              },
-            },
-        };
+        const {doctype, doctypeValue, subjects,expanded,category, addDefaultTags, addTags } = this.state;
           
         return(
             <div className={classes.container}>
                 <div className={classes.titleComponenet}>
-                    <TextField
-                        id="outlined-with-placeholder"
-                        label="문서 제목"
-                        placeholder="문서의 제목을 입력하세요."
-                        className={classes.titleField}
-                        margin="normal"
-                        variant="outlined"
-                        />
+                    <TextField id="outlined-with-placeholder" label="문서 제목" placeholder="문서의 제목을 입력하세요." className={classes.titleField} margin="normal" variant="outlined" />
                 </div>
                 <div className={classes.checkBoxComponent}>
                     <RadioList datas={doctype} selectValue={doctypeValue} onChange={this.handleRadioChange("doctypeValue")}/>
@@ -239,104 +159,55 @@ class DocumentWrite extends React.Component{
                 <div className={classes.selectComponent}>
                     <FormControl className={classes.selectControl}>
                         <InputLabel htmlFor="select-multiple-checkbox">{doctypeValue} 목차</InputLabel>
-                        <Select
-                        MenuProps={MenuProps}
-                        >
-                          {subjects[doctypeValue] && Object.keys(subjects[doctypeValue]).map((title,index)=>{
-                                return(
-                                    <MenuItem key={index} value={title} >
-                                    <Checkbox onChange={this.handleSelectChange(title)} checked={subjects[doctypeValue][title]} />
-                                    <ListItemText primary={title} />
-                                    </MenuItem>
-                            )
-                        })}
+                        <Select value={subjects[doctypeValue]} onChange={this.handleSelectChange} >
+                        {subjects[doctypeValue] && Object.keys(subjects[doctypeValue]).map((title,index)=>{ return(
+                            <MenuItem key={index} value={title} >
+                            <Checkbox checked={subjects[doctypeValue][title]} />
+                            <ListItemText primary={title} />
+                            </MenuItem>
+                        )})}
                         </Select>
                     </FormControl>
-                  </div>
-                  <div  className={classes.subjectComponent}>
+                </div>
+                <div className={classes.subjectComponent}>
                     <div className={classes.subjectWrapper}>
                     {subjects[doctypeValue] && 
                     Object.keys(subjects[doctypeValue]).map((title,index)=>{
-                        if(subjects[doctypeValue][title])
-                            return (
-                                <ExpansionPanel
-                                    className={classes.subjectField}
-                                    key={index} square
-                                    expanded={expanded === `panel${index}`}
-                                    onChange={this.handleSubjectBoxChange(`panel${index}`)}
-                                    >
-                                    <ExpansionPanelSummary>
-                                        <Typography>{title}</Typography>
-                                    </ExpansionPanelSummary>
-                                    <ExpansionPanelDetails>
-                                            <InputBase 
-                                                label="content"
-                                                multiline
-                                                rows="15"
-                                                fullWidth
-                                                value={content[title]}
-                                                onBlur={this.handleContentBlur(title)}
-                                                placeholder="내용 입력"
-                                                className={classes.contentField}
-                                            />
-                                    </ExpansionPanelDetails>
-                                </ExpansionPanel>
-                            )
-                      
+                        if(subjects[doctypeValue][title]) return (
+                            <ExpansionPanel key={index} onChange={this.handleSubjectBoxChange(`panel${index}`)} className={classes.subjectField} square expanded={expanded === `panel${index}`} >
+                                <ExpansionPanelSummary>
+                                    <Typography>{title}</Typography>
+                                </ExpansionPanelSummary>
+                                <ExpansionPanelDetails>
+                                    <InputBase onBlur={this.handleContentBlur(title)} className={classes.contentField}label="content" multiline rows="15" fullWidth placeholder="내용 입력" />
+                                </ExpansionPanelDetails>
+                            </ExpansionPanel>
+                        )
                     })}
-                </div>
+                    </div>
                 </div>
                 <div className={classes.checkBoxComponent}>
                     <CheckBoxList datas={category} onChange={this.handleCheckBoxChange("category")}/>
                 </div>
                 <div className={classes.tagComponent}>
                     <div className={classes.tagWrapper}>
-                        <Paper className={classes.tagPaper}>
-                        
-                        <p style={{padding:20}}>태그내역</p>
-                        {addDefaultTags.map((data, index)=>{
-                            return(
-                                <Chip
-                                key={index}
-                                label={data}
-                                className={classes.chip}
-                                component="a"
-                                href="#chip"
-                                clickable
-                                variant="outlined"
-                                color="primary"
-                                />
-                            )
-                        })} 
-                        {addTags.map((data, index)=>{
-                            return(
-                                <Chip
-                                key={index}
-                                label={data}
-                                className={classes.chip}
-                                component="a"
-                                href="#chip"
-                                clickable
-                                variant="outlined"
-                                color="primary"
-                                onDelete={this.handleTagDelete(data)}
-                                />
-                            )
-                        })}
+                        <Paper className={classes.tagPaper}>                        
+                            <div className={classes.tagPaperTitle}>태그내역</div>
+                            <div className={classes.chipsField}>
+                                {addDefaultTags.map((data, index)=>{ return(
+                                    <Chip key={index} label={data} className={classes.chip} component="a" href="#chip" clickable variant="outlined" color="primary" />
+                                )})} 
+                                {addTags.map((data, index)=>{ return(
+                                    <Chip key={index} label={data} className={classes.chip} component="a" href="#chip" clickable variant="outlined" color="primary"  onDelete={this.handleTagDelete(data)}/>
+                                )})}
+                            </div>
                         </Paper>
-                    <p>
-                        <TextField
-                            maxLength="15"
-                            value={this.state.tagAddData} 
-                            onChange={this.handleTagAddFieldBlur}
-                            label="추가할 태그"
-                            placeholder="특징적인 것 입력"
-                            variant="outlined"
-                        />
-                        <Button  onClick={this.handleTagAddButtonClick} variant="contained" color="primary"  className={classes.tagAddButton}  variant="outlined">추가</Button>
-                        <br/><br/>
-                        <Button  onClick={this.handleSaveButtonClick} variant="contained" color="primary">저장</Button>
-                    </p>
+                         <p>
+                            <TextField maxLength="15" value={this.state.tagAddData} onChange={this.handleTagAddFieldBlur} label="추가할 태그" placeholder="특징적인 것 입력" variant="outlined" />
+                            <Button  onClick={this.handleTagAddButtonClick} variant="contained" color="primary"  className={classes.tagAddButton}  variant="outlined">추가</Button>
+                            <br/><br/>
+                            <Button  onClick={this.handleSaveButtonClick} variant="contained" color="primary">저장</Button>
+                        </p>
                    </div>
                 </div>
             </div>
@@ -344,5 +215,82 @@ class DocumentWrite extends React.Component{
     }
 
 }
+
+
+const styles = theme =>({
+    container:{
+        margin : 10,
+    },
+    titleField: {
+        marginTop : 30,
+        marginLeft: theme.spacing.unit*2,
+        marginRight: theme.spacing.unit*2,
+        width:1200,
+    },
+    titleComponenet:{
+        display:'flex',
+        justifyContent :'center',
+    },
+    selectComponent:{
+        display:'flex',
+        justifyContent :'center',
+        margin:20,
+    },
+    selectControl: {
+        margin : theme.spacing.unit*2,
+        width:800,
+    },
+    selectMenu:{
+        maxHeight: 48 * 4.5 + 8,
+        width: 250,
+    },
+    subjectComponent :{
+        display:'flex',
+        justifyContent:'center'
+    },
+    subjectWrapper :{
+        width:1200,
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    subjectField:{
+        width:'100%',
+    },
+    contentField:{
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    checkBoxComponent:{
+        display:'flex',
+        justifyContent:'center',
+        margin : theme.spacing.unit,
+        marginTop : 30,
+    },
+    tagComponent:{
+        display:'flex',
+        justifyContent:'center',
+    },
+    tagWrapper:{
+        width:1200,
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    tagPaper:{
+        width:'100%',
+    },
+    tagPaperTitle:{
+        paddingLeft:30,
+        paddingTop:20,
+    },
+    tagAddButton:{
+        height:58,
+    },
+    chipsField:{
+        padding:20,
+    },
+    chip: {
+        margin: theme.spacing.unit,
+    },
+});
 
 export default withStyles(styles)(DocumentWrite);
